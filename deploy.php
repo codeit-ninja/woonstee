fe93abdaea4f97641bf9cab7c55cfdeb69dbabf0
codeitninja <richard@codeit.ninja>
2023-11-08 16:19:57 +0100

    Design changes

--- deploy.php
+++ deploy.php
@@ -1,11 +1,7 @@
 <?php
 namespace Deployer;
 
-require 'recipe/common.php';
-require 'vendor/florianmoser/bedrock-deployer/recipe/bedrock_valetplus_db.php';
-require 'vendor/florianmoser/bedrock-deployer/recipe/bedrock_env.php';
-require 'vendor/florianmoser/bedrock-deployer/recipe/bedrock_misc.php';
-require 'vendor/florianmoser/bedrock-deployer/recipe/filetransfer.php';
+require '/home/codeit/.config/composer/deployer/recipe/bedrock.php';
 
 // Config
 
@@ -15,19 +11,12 @@ add('shared_files', ['.env']);
 add('shared_dirs', ['web/app/uploads']);
 add('writable_dirs', ['web/app/uploads']);
 
-set( 'sync_dirs', [
-    dirname( __FILE__ ) . '/web/app/uploads/' => '{{deploy_path}}/shared/web/app/uploads/',
-] );
-
-set( 'local_root', dirname( __FILE__ ) );
-
 // Hosts
 
 host('192.168.2.2')
     ->set('remote_user', 'codeit')
-    ->set('deploy_path', '/srv/sites/dewoonsteetiel')
-    ->set('release_path', '/srv/sites/dewoonsteetiel/current');
+    ->set('deploy_path', '/srv/sites/dewoonsteetiel');
 
 // Hooks
 
-after('deploy:failed', 'deploy:unlock');
+// after('deploy:failed', 'deploy:unlock');
--- web/app/themes/understrap-child/templates/blocks/hero-banner.php
+++ web/app/themes/understrap-child/templates/blocks/hero-banner.php
@@ -12,9 +12,9 @@ if( $block['fullheight'] ) {
 ?>
 <div class="block block-hero-banner <?php echo join( ' ', $classes ); ?>" style="--codeit-banner-bg: <?php echo $block['background_color']; ?>">
     <div class="container h-100">
-        <div class="row align-items-center h-100 g-5">
-            <div class="col-lg-6 order-1 order-lg-0" data-aos="fade-right">
-                <div class="d-flex flex-column align-items-baseline gap-4">
+        <div class="row align-items-start h-100 g-5">
+            <div class="col-lg-8 order-1 order-lg-0" data-aos="fade-right">
+                <div class="d-flex flex-column align-items-baseline gap-4 block-hero-banner-content">
                     <div class="block-hero-banner-heading"><?php echo esc_html( $block['heading'] ); ?></div>
                     <div class="block-hero-banner-text"><?php echo esc_html( $block['text'] ); ?></div>
                     <?php if( $block['use_button'] ) : ?>
@@ -22,9 +22,9 @@ if( $block['fullheight'] ) {
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="col-lg-6 order-0 order-lg-1 mb-4 mb-lg-0" data-aos="fade-left">
-                <?php get_template_part('global-templates/image', null, ['image_ID' => 844]); ?>
-            </div>
+            <!-- <div class="col-lg-6 order-0 order-lg-1 mb-4 mb-lg-0" data-aos="fade-left">
+                <?php get_template_part('global-templates/image', null, ['image_ID' => $block['image']]); ?>
+            </div> -->
         </div>
     </div>
 </div>
