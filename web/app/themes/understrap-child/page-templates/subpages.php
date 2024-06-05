3074d19cc9bd9c2c23e4a2fc87b5f7a8e5a907f8
codeitninja <richard@codeit.ninja>
2024-06-05 15:38:03 +0200

    Design finalization subpages template

--- web/app/themes/understrap-child/page-templates/subpages.php
+++ web/app/themes/understrap-child/page-templates/subpages.php
@@ -20,7 +20,7 @@ $children = get_pages('child_of=' . 1362 . '&sort_column=menu_order');
 
     <div class="container mb-5">
         <div class="row gx-5">
-            <div class="col-md-3 sidebar-left">
+            <div class="col-md-4 col-lg-3 sidebar-left mb-5 mb-md-0">
                 <ul class="nav nav-sidebar flex-column gap-1">
                     <?php foreach( $children as $child ) : ?>
                         <li class="<?php echo $child->ID === get_the_ID() ? 'active' : ''; ?>">
@@ -29,22 +29,24 @@ $children = get_pages('child_of=' . 1362 . '&sort_column=menu_order');
                     <?php endforeach; ?>
                 </ul>
             </div>
-            <div class="col-md-9">
+            <div class="col-md-8 col-lg-9">
                 <main class="site-main" id="main" role="main">                    
                     <?php if( has_post_thumbnail() ) : ?>
-                        <picture style="float: left;">
+                        <picture style="float: left;" class="mb-md-4 mb-lg-1">
                             <?php the_post_thumbnail( array(350) ); ?>
                         </picture>
                     <?php endif; ?>
                     
                     <h1><?php the_title(); ?></h1>
-                    <!-- Render blocks -->
-                    <?php get_template_part('loop-templates/blocks/block', 'editor'); ?>
+                    <?php the_content(); ?>
                         
                 </main>
             </div>
         </div>
     </div>
+    
+    <!-- Render blocks -->
+    <?php get_template_part('loop-templates/blocks/block', 'editor', array('content' => false)); ?>
 </div>
 
 <?php
